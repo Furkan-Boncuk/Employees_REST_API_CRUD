6efc3d2bfe4a2e0f41d5b6cc5bbdeaf2921773a8
Furkan Boncuk <furkanboncuk2000@gmail.com>
2023-06-15 09:05:32 +0300

    search name,city, age, salary Eklendi.

--- demo/src/main/java/com/EmployesAPICRUD/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/EmployesAPICRUD/demo/controller/EmployeeController.java
@@ -2,16 +2,16 @@ package com.EmployesAPICRUD.demo.controller;
 
 import com.EmployesAPICRUD.demo.model.Employee;
 import com.EmployesAPICRUD.demo.repository.EmployeeRepository;
-import com.EmployesAPICRUD.demo.service.EmployeeService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -20,8 +20,6 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @Autowired
-    private EmployeeService employeeService;
 
     @PostMapping("/employees")
     public String createNewEmployee(@RequestBody Employee employee) {
@@ -75,12 +73,37 @@ public class EmployeeController {
         return "Employee Deleted Successfully..";
     }
 
+    @GetMapping("/employees/search")
+    public ResponseEntity<List<Employee>> searchEmployees(
+            @RequestParam(value = "name",required = false) String name,
+            @RequestParam(value="city",required = false) String city,
+            @RequestParam(value="age",required = false) Integer age,
+            @RequestParam(value="salary",required = false) Float salary) {
 
-    // !!!!!!!!
-    @GetMapping("/api/employees")
-    public ResponseEntity<List<Employee>> searchEmployeesByCity(@RequestParam("city") String city) {
-        List<Employee> employees = employeeService.getEmployeeByCity(city);
-        return new ResponseEntity<List<Employee>>(employees,HttpStatus.OK);
+        List<Employee> employees;
+
+        if(name!=null) {
+           Employee employee = employeeRepository.findEmployeeByName(name);
+           if(employee!=null) {
+               return new ResponseEntity<>(Collections.singletonList(employee), HttpStatus.OK);
+           } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+           }
+        } else if(city!=null) {
+            employees = employeeRepository.findEmployeeByCity(city);
+        } else if(age!=null) {
+            employees = employeeRepository.findEmployeeByAge(age);
+        } else if(salary!=null) {
+            employees = employeeRepository.findEmployeeBySalary(salary);
+        } else {
+            return new ResponseEntity<>(employeeRepository.findAll(),HttpStatus.OK);
+        }
+
+        if(employees.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(employees,HttpStatus.NOT_FOUND);
     }
 
 
--- demo/src/main/java/com/EmployesAPICRUD/demo/repository/EmployeeRepository.java
+++ demo/src/main/java/com/EmployesAPICRUD/demo/repository/EmployeeRepository.java
@@ -2,16 +2,19 @@ package com.EmployesAPICRUD.demo.repository;
 
 import com.EmployesAPICRUD.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     List<Employee> findEmployeeByCity(String city);
 
+    Employee findEmployeeByName(String name);
+
+    List<Employee> findEmployeeBySalary(float salary);
+
+    List<Employee> findEmployeeByAge(int age);
 }
